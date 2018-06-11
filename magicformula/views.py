bbdff15b85b52a02412913deb78171fccdcf7fa8
Toni <toni@MacBook-Pro-de-Toni.local>
2018-06-12 00:30:00 +0200

    Incluido fichero css base Dividido plantilla en base y cuerpo para cada pagina

--- magicformula/views.py
+++ magicformula/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Market, Company
 
 # Create your views here.
+def magicformula(request):
+	market = Market.objects.get()
+	companies = market.companies.all()
+	return render(request, 'magicformula/magic.html', {'market': market, 'companies':companies})
+
+
