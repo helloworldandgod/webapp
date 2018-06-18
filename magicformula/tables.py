8a24796d4e2b12754c2ccf885c178818501daef0
Toni <toni@MacBook-Pro-de-Toni.local>
2018-06-19 00:12:10 +0200

    Uso de libreria django-tables2 para representar tablas

---
+++ magicformula/tables.py
@@ -0,0 +1,9 @@
+import django_tables2 as tables
+from .models import Company
+
+class CompaniesTable(tables.Table):
+	class Meta:
+		model = Company
+		template_name = 'django_tables2/bootstrap.html'
+
+
--- magicformula/views.py
+++ magicformula/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Market, Company
+from django_tables2 import RequestConfig
+from .tables import CompaniesTable
 
 # Create your views here.
 def magicformula(request):
 	market = Market.objects.get()
 	companies = market.companies.all()
-	return render(request, 'magicformula/magic.html', {'market': market, 'companies':companies})
+	table = CompaniesTable(market.companies.all())
+	return render(request, 'magicformula/magic.html', {'market': market, 'table':table})
 
 
